a7e460ad677633403eb720fbe50e0676092a7d64
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-20 23:37:42 +0200

    Added sidebar with categories to the shop component

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import './Shop.css'
 
+function Categories(props) {
+    const { categories, selectCategory } = props
+
+    return (
+        categories.map(category => {
+            const uppercased = category.charAt(0).toUpperCase() + category.slice(1);
+            return (
+                <div className='category'>
+                    <button name={category} onClick={selectCategory}>{uppercased}</button>
+                </div>
+            )
+        })
+    )
+}
+
 function ProductList(props) {
     const {products} = props
 
     return (
         <div className='product-list flex'>
             {products.map(product => {
-                console.log(product)
                 return (
                     <div key={product.id} className='product'>
                         <div className='product-image-container flex justify-center align-center'>
@@ -24,10 +38,15 @@ function ProductList(props) {
 
 function Shop() {
     const [products, setProducts] = useState([])
+    const [categories, setCategories] = useState([])
 
     useEffect(() => {
         fetchItems()
     },[])
+    
+    useEffect(() => {
+        getCategories()
+    },[products])
 
     async function fetchItems() {
         const response = await fetch ('https://fakestoreapi.com/products')
@@ -35,12 +54,38 @@ function Shop() {
         setProducts(data)
     }
 
+    function selectCategory(e) {
+        console.log(e.target.name)
+    }
+
+    function getCategories() {
+        let newCategories = []
+
+        products.forEach(product => {
+            const productCategory = product.category
+
+            if (!newCategories.includes(productCategory)) {
+                newCategories.push(productCategory)
+            } 
+        })
+
+        setCategories(newCategories)
+    }
+
     return (
         <div className='shop-container flex'>
-            <div>
-
+            <div className='sidebar'>
+                <header>
+                    <h2>CATEGORIES</h2>
+                </header>
+                <div className='category-list'>
+                    <Categories categories={categories} selectCategory={selectCategory}/>
+                </div>
             </div>
             <div>
+                <header className='products-header'>
+                    <h2>Showing all Products</h2>
+                </header>
                 <ProductList products={products} />
             </div>
         </div>
