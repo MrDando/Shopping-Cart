8ee4f499ece842082690cbbcc36d8ed090c3b148
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-20 14:04:45 +0200

    Setup basic layout and style for webpage header

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 function Nav() {
     return (
-        <header className='main-header flex'>
-            <h2>Navigation</h2>
+        <header className='main-header flex align-center'>
+            <div className='logo'>
+                <Link className='nav-link' to='/'><h1>Home</h1></Link>
+            </div>
+            <nav>
+                <ul className='nav-links'>
+                    <li><Link className='nav-link' to='/shop'>Shop</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
