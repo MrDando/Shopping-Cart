9cb4a9ff5495e18edf738f548e06bcd3f6f577ac
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-21 19:57:57 +0200

    Updated style and functionality for ProductDetails component

--- src/Components/Shop/ProductDetails.js
+++ src/Components/Shop/ProductDetails.js
@@ -1,11 +1,34 @@
 import { useState ,useEffect } from 'react'
 
 function ProductDetails(props) {
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState({title: ''})
+    const [quantity, setQuantity] = useState(1)
+    const [quantityError, setQuantityError] = useState(false)
 
     useEffect(() => {
         fetchItem()
     }, [])
+
+    function updateQuantity(e) {
+        let newQuantity = ''
+
+        if (e.target.value !== '') {
+            newQuantity = Number(e.target.value)
+        }
+        
+        console.log('new value', newQuantity)
+        
+        if (!Number.isInteger(newQuantity)) {
+            setQuantityError(true)
+        } else {
+            setQuantityError(false)
+        }
+
+        if (newQuantity >= 1 || newQuantity === '') {
+            setQuantity(newQuantity)
+        }
+
+    }
 
     async function fetchItem() {
         const productId = props.location.state.productId
@@ -13,29 +36,29 @@ function ProductDetails(props) {
         const data = await response.json()
         setProduct(data)
     }
-    console.log(product)
-    return (
-        <div className='product-details'>
-            <div className='image-container flex justify-center align-center'>
-                <img src={product.image}></img>
-            </div>
-            <div className='info flex column'>
-                <header>
-                    <h2 className='product-title'>{product.title}</h2>
-                </header>
-                <p>$ {product.price}</p>
-                <div className='flex buy-options'>
-                    <div className='flex column'>
-                        <p>Quantity</p>
-                        <input type='number'></input>
-                    </div>
-                    <div>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-                <p>{product.description}</p>
-            </div>
 
+    return (
+        <div className='product-details-container'>
+            <div className='product-details'>
+                <div className='image-container flex justify-center align-center'>
+                    <img src={product.image}></img>
+                </div>
+                <div className='info flex column'>
+                    <header>
+                        <h2 className='product-title'>{product.title.toUpperCase()}</h2>
+                    </header>
+                    <p className='price'>$ {product.price}</p>
+                    <div className='flex column buy-options'>
+                        <p>Quantity:</p>
+                        <div className='flex'> 
+                            <input type='number' value={quantity} onChange={updateQuantity}></input>
+                            <button>ADD TO CART</button>
+                        </div>
+                    {quantityError ? <p className='quantity-error'>You can only enter whole numbers here. Please correct your input.</p> : null}
+                    </div>
+                    <p className='product-description'>{product.description}</p>
+                </div>
+            </div>
         </div>
     )
 }
